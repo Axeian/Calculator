6e477e982f67a5f75ce45892f3f6501887124a60
Abhishek Saxena <abhishek05101998@gmail.com>
2020-09-07 08:55:35 +0530

    Updates

---
+++ precache-manifest.2cc274e01b3eab53b6ec4897a014e00e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fddbbe715f6d59ffc0392f9a419213e2",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "a63442ada8de414313ec",
+    "url": "/Calculator/static/css/2.6e90b12a.chunk.css"
+  },
+  {
+    "revision": "7ff8e45dad247544492f",
+    "url": "/Calculator/static/css/main.d536f249.chunk.css"
+  },
+  {
+    "revision": "a63442ada8de414313ec",
+    "url": "/Calculator/static/js/2.58dece43.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Calculator/static/js/2.58dece43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff8e45dad247544492f",
+    "url": "/Calculator/static/js/main.b695666a.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  },
+  {
+    "revision": "7c6e146fe91c1b2633316a6aee97a9af",
+    "url": "/Calculator/static/media/digital-7 (italic).7c6e146f.ttf"
+  }
+]);
